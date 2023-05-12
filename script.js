67b4f0e40d68d491993733dd893b8f0bccb235ec
I549511 <johanna.deike@sap.com>
2023-05-12 15:29:20 +0200

    some other changes

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-console.log("hello world");
+/**@type {HTMLButtonElement} */
+let addHobby;
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM CONTENT LOADED");
+  addHobby = document.getElementById("submitNewHobby");
+  addHobby.addEventListener("click", addNewHobby);
 });
 
 const addNewHobby = () => {
   const newHobby = document.getElementById("NewHobby").value;
 
-  let node = document.createElement("li")
-  node.appendChild(document.createTextNode(newHobby));
-  
-  document.getElementById("hobbyList").appendChild(node);
+  if (newHobby) {
+    let node = document.createElement("li");
+    node.innerText = newHobby;
+
+    document.getElementById("hobbyList").appendChild(node);
+  }
 };
